fe796191d4df0686847d74d7fe8dc0298e08b7ff
Maruf <maruf.csea1@gmail.com>
2020-04-07 01:44:18 -0400

    Log file added

--- spmv_cuda/main.cu
+++ spmv_cuda/main.cu
@@ -186,7 +186,7 @@ void call_cusparse_ref(int m, int n, int nnz,
         fprintf(resultCSV, "Name,M,N,AvgTime,TotalRun,NonZeroPerRow,NonZeroElements,Bandwidth,Flops,ValueType,Type,Strides,TransactionByte,WordSize\n");
     }
 
-    fprintf(resultCSV, "%s,%d,%d,%10.6lf,%d,%lf,%d,%lf,%lf,%d,%s\n", matName, m, n, cusparseTime, NUM_RUN, (double) nnz / m,
+    fprintf(resultCSV, "%s,%d,%d,%10.6lf,%d,%lf,%d,%lf,%lf,%d,%s,%ld,%d,%d\n", matName, m, n, cusparseTime, NUM_RUN, (double) nnz / m,
             nnz, gb / (1.0e+6 * cusparseTime), gflop / (1.0e+6 * cusparseTime), sizeof(value_type), "CUSPARSE", strideCounts,
             TRANSACTION_BYTE, TRANSACTION_BYTE/ sizeof(value_type));
     if (fclose(resultCSV) != 0) {
@@ -364,7 +364,7 @@ void call_cusp_ref(int m, int n, int nnz,
         fprintf(resultCSV, "Name,M,N,AvgTime,TotalRun,NonZeroPerRow,NonZeroElements,Bandwidth,Flops,ValueType,Type,Strides,TransactionByte,WordSize\n");
     }
 
-    fprintf(resultCSV, "%s,%d,%d,%10.6lf,%d,%lf,%d,%lf,%lf,%d,%s\n", matName, m, n, cuspTime, NUM_RUN, (double) nnz / m,
+    fprintf(resultCSV, "%s,%d,%d,%10.6lf,%d,%lf,%d,%lf,%lf,%d,%s,%ld,%d,%d\n", matName, m, n, cuspTime, NUM_RUN, (double) nnz / m,
             nnz, gb / (1.0e+6 * cuspTime), gflop / (1.0e+6 * cuspTime), sizeof(value_type), "CUSP", strideCounts,
             TRANSACTION_BYTE, TRANSACTION_BYTE/ sizeof(value_type));
     if (fclose(resultCSV) != 0) {
